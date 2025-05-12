87855aab274e875a0c51e6efa44c2c6f759040e8
akadirgns <asd51abd@gmail.com>
2025-05-12 13:31:02 +0300

    Delete javscript directory

--- javscript/kayıt.js
+++
@@ -1,8 +0,0 @@
-var degerler = new URLSearchParams(window.location.search);
-var ad = degerler.get('isim');
-var eposta = degerler.get('eposta');
-
-document.write("<p>Kaydınız Oluşturulmuştur. Tebrikler :) <3 <3 <3 <3</p>");
-document.write("<p>Adınız: " + ad + "</p>");
-document.write("<p> Eposta Adresiniz : " + eposta + "</p>");
-document.write("<p> Olarak Kaydedildiniz: " + ad + " </p>");
